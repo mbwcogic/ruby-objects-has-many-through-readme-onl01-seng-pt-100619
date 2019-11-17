a2fb40c6f919289bb0504b06afac576ca7d34cfe
Michael Waldo <mbwcogic@yahoo.com>
2019-11-17 16:53:09 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -15,4 +15,9 @@ attr_accessor :name, :yrs_experience
  def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
+  def meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+  end
+end
 end
